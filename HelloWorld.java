6345748594532052febd879aa2c929cadf8e3e3f
zhaochengfeng3 <zhaochengfeng3@tom.com>
2019-10-17 20:49:44 +0800

    update commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,9 +6,10 @@ class HelloWorld
     System.out.println("HelloWorld2");
     System.out.println("HelloWorld3");
     System.out.println("helloworld4");
-    System.out.println("helloworld5");
-
+    System.out.println("helloworld5"); 
     System.out.println("helloworld6");
-
+    System.out.println("helloworld7");
+    System.out.println("helloworld8");
+    System.out.println("helloworld9");
  }
 }
