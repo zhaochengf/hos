6fc1ea114e0d0711877f9052ceff2d61a8edc666
zhaochengf <zhaochengfeng2@163.com>
2019-10-17 19:56:48 +0800

    dev first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,8 @@ class HelloWorld
     System.out.println("HelloWorld2");
     System.out.println("HelloWorld3");
     System.out.println("helloworld4");
-
- }
+    System.out.println("helloworld5");
+    System.out.println("helloworld6");
+}
 
 }
